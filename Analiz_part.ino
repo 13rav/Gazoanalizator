ab0010354d7325b29248f86a07541c8aba54d794
13rav <44643207+13rav@users.noreply.github.com>
2019-05-30 23:18:16 +0300

    Update Analiz_part.ino

--- Analiz_part.ino
+++ Analiz_part.ino
@@ -15,14 +15,14 @@ void setup() {
 void loop() {
   sensorValue = analogRead(MQanalogPin);
 
-  if (sensorValue) rangeValue = 1;
-  if (sensorValue) rangeValue = 1;
-  if (sensorValue) rangeValue = 1;
-  if (sensorValue) rangeValue = 1;
-  if (sensorValue) rangeValue = 1;
-  if (sensorValue) rangeValue = 1;
-  if (sensorValue) rangeValue = 1;
-  if (sensorValue) rangeValue = 1;
+  if (sensorValue < 2500) rangeValue = 1;
+  if (sensorValue > 2500 && sensorValue < 5001) rangeValue = 2;
+  if (sensorValue > 5000 && sensorValue < 7501) rangeValue = 3;
+  if (sensorValue > 7500 && sensorValue < 10001) rangeValue = 4;
+  if (sensorValue > 10000 && sensorValue < 12501) rangeValue = 5;
+  if (sensorValue > 12500 && sensorValue < 15001) rangeValue = 6;
+  if (sensorValue > 15000 && sensorValue < 17501) rangeValue = 7;
+  if (sensorValue > 17500 && sensorValue < 20001) rangeValue = 8;
 
  Serial.print(rangeValue);
  delay(100);
